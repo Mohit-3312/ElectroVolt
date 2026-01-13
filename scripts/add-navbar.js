721905c02e790bfb5cc432d5e321ebb0bd16ca78
Mohit-3312 <bhutiyamohit6@gmail.com>
2026-01-13 09:19:30 +0530

    added other html files and bootstrap css/js in local without CDN

--- scripts/add-navbar.js
+++ scripts/add-navbar.js
@@ -1,12 +1,45 @@
-// script.js
-function loadNavbar() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-holder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading the navbar:', error));
-}
+const navbarHTML = `
+  <nav class="navbar navbar-expand-lg bg-primary text-primary-emphasis">
+  <div class="container-fluid">
+    <p class="navbar-brand text-light fs-3 mt-3" disabled>ElectroVolt</p>
+    <form class="d-flex ps-3 gap-3" role="search">
+        <input class="form-control m-auto " type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+      <ul class="navbar-nav m-auto mb-2 mb-lg-0 gap-3 fs-5">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="index.html" >Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Products
+          </a>
+          <ul class="dropdown-menu bg-primary mx-auto my-0">
+            <li><a class="dropdown-item text-white " href="home-appliances.html">Home Appliances</a></li>
+            <li><a class="dropdown-item text-white " href="mobile.html">Mobile</a></li>
+            <li><a class="dropdown-item text-white " href="laptop.html">Laptop</a></li>
+            <li><a class="dropdown-item text-white " href="smartwatch.html">Smartwatch</a></li>
+            <li><a class="dropdown-item text-white " href="camera.html">Camera</a></li>
+            <li><a class="dropdown-item text-white " href="gaming-gadget.html">Gaming Gadgets</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="order.html">Order</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="about-us.html">About Us</a>
+        </li>
+      </ul>
+      <button type="button" class="btn btn-outline-light mt-lg-0 mt-3">Sign Up</button>
+    </div>
+  </div>
+</nav>
 
-// Run the function when the page loads
-window.onload = loadNavbar;
+`;
+
+document.getElementById('navbar-holder').innerHTML = navbarHTML;
